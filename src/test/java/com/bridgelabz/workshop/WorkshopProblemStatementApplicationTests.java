eebf0d86d7b4e4e6e842c0954cc111713ce73682
RuchirDixit <ruchirtd96@gmail.com>
2021-05-26 10:33:00 +0530

    [Ruchir]Added: Files

---
+++ src/test/java/com/bridgelabz/workshop/WorkshopProblemStatementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopProblemStatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
